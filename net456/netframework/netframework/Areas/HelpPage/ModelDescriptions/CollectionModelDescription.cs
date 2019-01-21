7464723b09b960749571856a071ac34516e2ca8e
DwaynesWorld <kthompson713@gmail.com>
2019-01-21 17:07:52 -0600

    renamed folder to match version

--- net456/netframework/netframework/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace netframework.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
